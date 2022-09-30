3ef228e7458fa80cc1b1f34ebcfc6ef9b6109fc7
vasil14 <vasil_v@hotmail.com>
2022-09-30 10:06:40 +0200

    Setting up the API call

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  if request.method == 'POST':
+    city = request.POST['city']
+    res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=d65ccc4049cb509934c24d876d131e0c').read()
+    json_data = json.loads(res)
+    data = {
+      'country_code': str(json_data['sys']['country']),
+      'coordinate': str(json_data['coord']['lon'])+ ' '+str(json_data['coord']['lat']),
+      'temp': str(json_data['main']['temp']),
+      'pressure': str(json_data['main']['pressure']),
+      'humidity': str(json_data['main']['humidity'])
+    }
+  else:
+    city = ''
+    data = {}
+  return render(request, 'index.html', {'city': city, 'data': data})
